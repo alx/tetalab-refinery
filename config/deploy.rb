8bdda49d1e662b8c12fe4b843faff408ff27f305
Alexandre Girard <alx.girard@gmail.com>
2011-04-10 18:12:32 +0200

    change deploy and add setup_load_paths

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,33 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require "bundler/capistrano"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+default_run_options[:pty] = true
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :application, "refinery"
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :deploy_to,   "/var/www/#{application}"
+set :user,        "webadmin"
+set :port,        22101
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+set :scm,               :git
+set :repository,        "git@10.10.10.1:jekyll.git"
+set :scm_user,          "git"
+set :branch,            "master"
+set :deploy_via,        :remote_cache
+
+role :web, "www.tetalab.org"
+role :app, "www.tetalab.org"
+role :db, "www.tetalab.org"
+
+# ========================
+#
+# App tasks
+#
+# ========================
+
+namespace :deploy do
+  task :start do ; end
+    task :stop do ; end
+    task :restart, :roles => :app, :except => { :no_release => true } do
+      run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+    end
+end
