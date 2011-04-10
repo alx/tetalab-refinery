6ee9d0e3a4214c8c0fc8d4ee768918af04359663
Alexandre Girard <alx.girard@gmail.com>
2011-04-10 17:31:31 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BetaTetalabOrg::Application.config.secret_token = 'b061069670392ec8857507b541bf53353f3ced32977aa4e503c34c0288d0764f763b9ad9a0af30f9d66b257757aa843f02455a1bd70ef7e8dd5abee178498f03'
