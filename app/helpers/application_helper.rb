3747ef35a57fbd3a57007e7c6aeacc003a84a481
Alexandre Girard <git@alexgirard.com>
2011-09-20 14:34:09 +0200

    remove blog_teaser

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,2 @@
 module ApplicationHelper
-
-  def blog_post_teaser(post)
-    if post.respond_to?(:custom_teaser) && post.custom_teaser.present?
-     post.custom_teaser.html_safe
-    else
-     truncate(post.body, {
-       :length => RefinerySetting.find_or_set(:blog_post_teaser_length, 250),
-       :preserve_html_tags => true
-      }).html_safe
-    end
-  end
-  
 end
